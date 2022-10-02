daf716ac9cb66be658648d7b2f48c5119d044120
Marwin Dollendorf <marwin.dollendorf@gmail.com>
2022-10-02 18:33:39 +0200

    Removed helper script.

--- Assets/Scripts/Rename/main.py
+++
@@ -1,9 +0,0 @@
-import os
-
-folder = "C:\\Users\\juliu\\Documents\\GameJam\\20220930_GameJam\\Assets\\Characters\\png\\walkcycle\\Controller"
-prefix = "walkcycle"
-
-files = os.listdir(folder)
-
-for file in files:
-    os.rename(f"{folder}\\{file}", f"{folder}\\{prefix}_{file}")
